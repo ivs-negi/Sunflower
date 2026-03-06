8ae161292a800015241687aa54053ec2756cfa9a
Vijay Singh Negi <ivsnegi8126@gmail.com>
2026-03-06 13:52:48 +0530

    update

--- src/main/java/com/sunflower/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sunflower/exception/GlobalExceptionHandler.java
@@ -2,7 +2,6 @@ package com.sunflower.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
