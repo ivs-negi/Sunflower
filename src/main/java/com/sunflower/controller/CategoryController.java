d11753428ab41a6436abdfe583310633160ed159
Vijay Singh Negi <ivsnegi8126@gmail.com>
2026-03-11 11:37:16 +0530

    yeah

--- src/main/java/com/sunflower/controller/CategoryController.java
+++ src/main/java/com/sunflower/controller/CategoryController.java
@@ -4,7 +4,6 @@ import com.sunflower.model.Category;
 import com.sunflower.service.CategoryService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
