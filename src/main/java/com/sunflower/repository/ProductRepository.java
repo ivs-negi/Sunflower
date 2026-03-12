8eadd8ab665307fc9abc9a2226e9c7113568eb46
Vijay Singh Negi <ivsnegi8126@gmail.com>
2026-03-12 14:12:19 +0530

    complete category

--- src/main/java/com/sunflower/repository/ProductRepository.java
+++ src/main/java/com/sunflower/repository/ProductRepository.java
@@ -16,10 +16,4 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByProductNameContainingIgnoreCase(String keyword, Pageable pageable);
 
     boolean existsByProductName(String productName);
-
-    Page<Product> findByCategoryId(Long categoryId, Pageable pageable);
-
-    Page<Product> findByPriceBetween(Double minPrice, Double maxPrice, Pageable pageable);
-
-    Page<Product> findByQuantityLessThan(Integer threshold, Pageable pageable);
 }
